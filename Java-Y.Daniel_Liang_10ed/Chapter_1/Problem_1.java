083b22b152474e0a2f6e47c3245bb7df2a9776a6
Adonay Mejia <mejiaadonay@gmail.com>
2016-03-06 23:55:06 -0500

    Problem 1.1

--- Java-Y.Daniel_Liang_10ed/Chapter_1/Problem_1.java
+++ Java-Y.Daniel_Liang_10ed/Chapter_1/Problem_1.java
@@ -4,7 +4,6 @@
 */
 	public class Problem_1 {
 		public static void main(String[] args){
-
 			System.out.println("Welcome to Java");
 			System.out.println("Welcome to Computer Science");
 			System.out.println("Programming is fun");
