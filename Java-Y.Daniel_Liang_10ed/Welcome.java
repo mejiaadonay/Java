ec519eff99f02afbebe6bc321e27c45b3ba9cb45
Adonay Mejia <mejiaadonay@gmail.com>
2016-03-07 14:09:31 -0500

    Update Chapter_01

--- Java-Y.Daniel_Liang_10ed/Welcome.java
+++
@@ -1,8 +0,0 @@
-// @autor: Jose Escobar Mejia
-// @Date: March 5, 2016
-public class Welcome {
-	public static void main(String[] args){
-		// Display a simple "Welcome World!" message!
-		System.out.println("Hello World!");
-	}
-}
