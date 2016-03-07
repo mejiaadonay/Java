068c7d6c384a593868f3bb3df4af729d321785f8
Adonay Mejia <mejiaadonay@gmail.com>
2016-03-07 14:26:36 -0500

    src update

---
+++ Java-Y.Daniel_Liang_10ed/src/Chapter_01/Exercise_02.java
@@ -0,0 +1,20 @@
+/*
+*	1.2 (Display three message) Write a program that displays "Welcome to Java" five times.
+*/
+
+/*
+*	@author: Jose Escobar Mejia
+*	@date: March 06, 2016
+*/
+
+public class Problem_2 {
+	/** This is the main method */
+	public static void main(String[] args){
+		// For loop to display the message five time
+		for(int i = 0; i < 5; i++){
+			System.out.println("Welcome to Java");
+		}
+	}
+}
+		
+	
