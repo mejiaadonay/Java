59bc85bccfcbcba685235803791725518cd59080
Adonay Mejia <mejiaadonay@gmail.com>
2016-03-07 10:56:58 -0500

    Solution 1.2 Exercise

--- Java-Y.Daniel_Liang_10ed/Chapter_1/Problem_1.java
+++ Java-Y.Daniel_Liang_10ed/Chapter_1/Problem_1.java
@@ -2,10 +2,16 @@
 	1.1 (Display three message) Write a program that displays "Welcome to Java", "Welcome to Computer Science",
 	and "Programming is fun".
 */
-	public class Problem_1 {
-		public static void main(String[] args){
-			System.out.println("Welcome to Java");
-			System.out.println("Welcome to Computer Science");
-			System.out.println("Programming is fun");
-		}
+
+/*
+	@author: Jose Escobar Mejia
+	@date: March 06, 2016
+*/
+
+public class Problem_1 {
+	public static void main(String[] args){
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Computer Science");
+		System.out.println("Programming is fun");
 	}
+}
