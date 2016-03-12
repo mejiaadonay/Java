e2a75364d3f05054a8946355fe1582c68ff359a6
Adonay Mejia <mejiaadonay@gmail.com>
2016-03-12 13:17:16 -0500

    Add Exercise_05 Solution

---
+++ Java-Y.Daniel_Liang_10ed/src/Chapter_01/Exercise_05.java
@@ -0,0 +1,15 @@
+/*
+*	(Computer expression) Write a program that displays the following table:
+*
+*						  9.5 X 4.5 - 2.5 X 3
+*						  -------------------
+*                             45.5 - 3.5
+*
+*		@author: Jose Escobar Mejia
+*		@date: March 12, 2016
+*/
+public class Exercise_05 {
+	public static void main(String[] args) {
+		System.out.println( ((9.5 * 4.5) - (2.5 * 3)) / (4.5 * 3.5) );
+	}
+}
