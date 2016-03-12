508f2fbc0e49b10928de7cc0809e4d946e735678
Adonay Mejia <mejiaadonay@gmail.com>
2016-03-12 13:38:54 -0500

    Add Exercise_06 Solution

--- Java-Y.Daniel_Liang_10ed/src/Chapter_01/Exercise_06.java
+++ Java-Y.Daniel_Liang_10ed/src/Chapter_01/Exercise_06.java
@@ -8,6 +8,7 @@
 */
 public class Exercise_06 {
 	public static void main(String[] args) {
-		System.out.println( 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 );
+		int summation = (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9);
+		System.out.println( "The summation of the serie: 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 is: " + summation);
 	}
 }
