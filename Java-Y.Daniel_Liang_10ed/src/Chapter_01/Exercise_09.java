6d51de5c88c4e093531c79ef0c2f76321b5c01c6
Adonay Mejia <mejiaadonay@gmail.com>
2016-03-14 22:28:43 -0400

    Add solution for Exercise_09

---
+++ Java-Y.Daniel_Liang_10ed/src/Chapter_01/Exercise_09.java
@@ -0,0 +1,23 @@
+/**
+*
+*      (Area and perimeter of a rectangle) Write a program that displays the area and 
+*		perimeter of a rectangle with the width of 4.5 and height of 7.9 using the following formula:
+*		
+*		area = width X height
+*
+*	@author: Jose Escobar Mejia
+*	@date: March 14, 2016
+*/
+
+ public class Exercise_09 {
+ 	public static void main(String[] args) {
+
+ 		final double width = 4.5;
+ 		final double height = 7.9;
+
+ 		double area = width * height;
+		
+		System.out.printf("The area of a rectangle with the width of 4.5 and height of 7.9 is: %.3f\n", area);
+ 	}
+ }
+ 
